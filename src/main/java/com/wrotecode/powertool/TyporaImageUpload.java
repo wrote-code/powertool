01de01249ace52b561bbafce7047bf9ebfe5d327
wrote-code <developer@noreply.email.com>
2023-09-26 02:16:22 +0800

    修复bug，适配typora上传参数

--- src/main/java/com/wrotecode/powertool/TyporaImageUpload.java
+++ src/main/java/com/wrotecode/powertool/TyporaImageUpload.java
@@ -1,6 +1,6 @@
 package com.wrotecode.powertool;
 
-import com.wrotecode.powertool.exception.CoreException;
+import com.wrotecode.powertool.exception.CoreRuntimeException;
 import com.wrotecode.powertool.utils.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +18,7 @@ import java.text.SimpleDateFormat;
 public class TyporaImageUpload {
     private static final Logger log = LoggerFactory.getLogger(TyporaImageUpload.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // https://support.typora.io/Upload-Image/#custom
         log.info("==========");
         if (args.length == 0) {
@@ -35,7 +35,8 @@ public class TyporaImageUpload {
         String server = System.getProperty("server");
         log.info("本地图床{}", fileDir);
         if (!FileUtils.directoryExists(fileDir)) {
-            throw new CoreException("请检查目标目录是否存在");
+            log.info("请检查文件是否存在");
+            throw new CoreRuntimeException("请检查文件是否存在");
         }
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
         String currentDate = format.format(System.currentTimeMillis());
@@ -49,6 +50,10 @@ public class TyporaImageUpload {
         // 图片访问路径，比如http://localhost:8080/20230926/typora.png
         StringBuilder urlBuilder = new StringBuilder();
         for (String file : args) {
+            if ("测试".equals(file)) {
+                // typora传入的第一个参数是测试
+                continue;
+            }
             String filename = FileUtils.getFileName(file);
             String targetFile = targetDir + File.separator + filename;
             FileUtils.copyFile(file, targetFile);
